73d3af7b8bc2dca46f670fe0dcdcdd440e3fe247
sagittaria <shuyang_x@hotmail.com>
2016-09-09 14:00:19 +0800

    set hbs as view engine

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,10 +12,11 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', __dirname+'/views');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+hbs.registerPartials(__dirname + '/views/partials');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
